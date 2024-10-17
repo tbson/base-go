764ee9c085852570ba906446f2e78f5770f2d373
Son Tran <tbson87@gmail.com>
2024-10-17 11:21:58 +0700

    Abstract away list paging method to easy for reusing.

--- backend/src/module/config/usecase/crudvariable/infra/repo.go
+++ backend/src/module/config/usecase/crudvariable/infra/repo.go
@@ -1,6 +1,7 @@
 package infra
 
 import (
+	"src/module/config/repo/common"
 	"src/module/config/repo/variable"
 	"src/module/config/schema"
 	"src/util/restlistutil"
@@ -25,46 +26,6 @@ func (r Repo) List(
 	options restlistutil.ListOptions,
 	searchableFields []string,
 ) (restlistutil.ListRestfulResult[schema.Variable], error) {
-	db := r.db
-	pageSize := restlistutil.DEFAULT_PAGE_SIZE
-	var items []schema.Variable
-	emptyResult := restlistutil.ListRestfulResult[schema.Variable]{
-		Items:      items,
-		Total:      0,
-		PageSize:   pageSize,
-		TotalPages: 0,
-		Pages: restlistutil.Pages{
-			Next: 0,
-			Prev: 0,
-		},
-	}
-	query := db.Model(new(*schema.Variable))
-
-	// Apply search logic
-	query = restlistutil.ApplySearch(query, options.Search, searchableFields)
-
-	// Apply filters
-	query = restlistutil.ApplyFilters(query, options.Filters)
-
-	// Apply order
-	query = restlistutil.ApplyOrder(query, options.Order)
-
-	// Count total records before pagination
-	total, err := restlistutil.GetTotalRecords(query)
-	if err != nil {
-		return emptyResult, err
-	}
-
-	// Apply paging
-	pagingREsult := restlistutil.ApplyPaging(query, options.Page, total)
-	query = pagingREsult.Query
-	pages := pagingREsult.Pages
-	totalPages := pagingREsult.TotalPages
-
-	// Fetch the results
-	result := query.Find(&items)
-	if result.Error != nil {
-		return emptyResult, result.Error
-	}
-	return restlistutil.ListRestfulResult[schema.Variable]{Items: items, Total: total, Pages: pages, PageSize: pageSize, TotalPages: totalPages}, nil
+	commonRepo := common.Repo[schema.Variable]{}.New(r.db)
+	return commonRepo.ListPaging(options, searchableFields)
 }
