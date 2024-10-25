13e37cfe11dda91f9928c21bf910e733e5c68708
Son Tran <tbson87@gmail.com>
2024-10-26 01:36:21 +0700

    Upgrade deps.

--- frontend/src/component/common/waiting.jsx
+++ frontend/src/component/common/waiting.jsx
@@ -1,10 +1,11 @@
-import * as React from "react";
-import { Spin } from "antd";
+import * as React from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from 'antd';
 
 export default function Waiting() {
     return (
         <div className="backdrop">
-            <Spin tip="Loading..." />
+            <Spin indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />} />
         </div>
     );
 }
