133dbc02ae46a19bbb295b740c734813001be180
Son Tran <tbson87@gmail.com>
2024-10-29 21:54:50 +0700

    Format URL.

--- backend/src/module/config/usecase/crudvariable/infra/urls.go
+++ backend/src/module/config/usecase/crudvariable/infra/urls.go
@@ -18,11 +18,35 @@ func RegisterUrls(e *echo.Group, pemMap ctype.PemMap) (*echo.Group, ctype.PemMap
 	g := e.Group(fmt.Sprintf("/%s/%s", module, useCaseGroup))
 	rr := routeutil.RegisterRoute(g, pemMap)
 
-	rr.Rbac("GET", "/", List, []string{profiletype.ADMIN, profiletype.STAFF}, fmt.Sprintf("Get %s list", useCaseGroupName))
-	rr.Rbac("GET", "/:id", Retrieve, []string{profiletype.ADMIN, profiletype.STAFF}, fmt.Sprintf("Get %s detail", useCaseGroupName))
-	rr.Rbac("POST", "/", Create, []string{profiletype.ADMIN}, fmt.Sprintf("Create %s", useCaseGroupName))
-	rr.Rbac("PUT", "/:id", Update, []string{profiletype.ADMIN}, fmt.Sprintf("Update %s", useCaseGroupName))
-	rr.Rbac("DELETE", "/:id", Delete, []string{profiletype.ADMIN}, fmt.Sprintf("Delete %s", useCaseGroupName))
-	rr.Rbac("DELETE", "/", DeleteList, []string{profiletype.ADMIN}, fmt.Sprintf("Delete list %s", useCaseGroupName))
+	rr.Rbac(
+		"GET", "/", List,
+		[]string{profiletype.ADMIN, profiletype.STAFF},
+		fmt.Sprintf("Get %s list", useCaseGroupName),
+	)
+	rr.Rbac(
+		"GET", "/:id", Retrieve,
+		[]string{profiletype.ADMIN, profiletype.STAFF},
+		fmt.Sprintf("Get %s detail", useCaseGroupName),
+	)
+	rr.Rbac(
+		"POST", "/", Create,
+		[]string{profiletype.ADMIN},
+		fmt.Sprintf("Create %s", useCaseGroupName),
+	)
+	rr.Rbac(
+		"PUT", "/:id", Update,
+		[]string{profiletype.ADMIN},
+		fmt.Sprintf("Update %s", useCaseGroupName),
+	)
+	rr.Rbac(
+		"DELETE", "/:id", Delete,
+		[]string{profiletype.ADMIN},
+		fmt.Sprintf("Delete %s", useCaseGroupName),
+	)
+	rr.Rbac(
+		"DELETE", "/", DeleteList,
+		[]string{profiletype.ADMIN},
+		fmt.Sprintf("Delete list %s", useCaseGroupName),
+	)
 	return e, pemMap
 }
