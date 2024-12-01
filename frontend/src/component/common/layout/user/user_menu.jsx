812968cfd21197bb67654af963676403de9919e0
Son Tran <tbson87@gmail.com>
2024-12-01 11:02:32 +0700

    Refactor to separate dutty of sso result and auth result, add avatar to auth result.

--- frontend/src/component/common/layout/user/user_menu.jsx
+++ frontend/src/component/common/layout/user/user_menu.jsx
@@ -1,122 +1,92 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { useNavigate, useLocation, Outlet, NavLink } from 'react-router-dom';
+import React, { useState } from 'react';
 import { t } from 'ttag';
-import { Layout, Menu, Row, Col, Flex } from 'antd';
-import {
-    MenuUnfoldOutlined,
-    MenuFoldOutlined,
-    UserOutlined,
-    TagsOutlined,
-    TeamOutlined
-} from '@ant-design/icons';
-import { LOGO_TEXT, DOMAIN } from 'src/const';
-import PemUtil from 'service/helper/pem_util';
+import { UserOutlined, LogoutOutlined } from '@ant-design/icons';
+import { Drawer, Avatar, Menu, Divider } from 'antd';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Util from 'service/helper/util';
+import StorageUtil from 'service/helper/storage_util';
 import NavUtil from 'service/helper/nav_util';
-import UserMenu from './user_menu';
-import styles from './styles.module.css';
 
-const { Header, Footer, Sider, Content } = Layout;
+function MenuHeading({ email, fullName, avatar }) {
+    return (
+        <div style={{ display: 'flex' }}>
+            <div style={{ width: 40 }}>
+                <Avatar src={avatar} icon={<UserOutlined />} />
+            </div>
+            <div style={{ flex: 1 }}>
+                <div>
+                    <strong>{fullName}</strong>
+                </div>
+                <div>{email}</div>
+            </div>
+        </div>
+    );
+}
 
-/**
- * UserLayout.
- */
-export default function UserLayout() {
+export default function UserMenu() {
+    const [open, setOpen] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
-    const [menuItems, setMenuItems] = useState([]);
-
-    const [collapsed, setCollapsed] = useState(false);
-    const toggle = () => {
-        setCollapsed(!collapsed);
-    };
-
-    useEffect(() => {
-        setMenuItems(getMenuItems());
-    }, []);
-
+    const logout = NavUtil.logout(navigate);
     const navigateTo = NavUtil.navigateTo(navigate);
 
-    const getMenuItems = () => {
-        const result = [];
+    const userInfo = StorageUtil.getUserInfo();
+    if (!userInfo) {
+        return null;
+    }
+    const { email, avatar, first_name, last_name } = userInfo;
+    const fullName = `${first_name} ${last_name}`;
 
-        result.push({ label: t`Profile`, key: '/', icon: <UserOutlined /> });
-        PemUtil.canView('crudrole') &&
-            result.push({
-                label: t`Role`,
-                key: `/account/role`,
-                icon: <TagsOutlined />
-            });
-        PemUtil.canView('cruduser') &&
-            result.push({
-                label: t`User`,
-                key: `/account/user`,
-                icon: <TeamOutlined />
-            });
-        return result;
+    const handleLogout = () => {
+        Util.toggleGlobalLoading();
+        logout();
+    };
+
+    const handleShow = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
     };
 
     return (
-        <Layout hasSider className={styles.wrapperContainer}>
-            <Sider
-                trigger={null}
-                breakpoint="lg"
-                collapsedWidth="34"
-                theme="dark"
-                collapsible
-                collapsed={collapsed}
-                onBreakpoint={(broken) => {
-                    setCollapsed(broken);
-                }}
+        <>
+            <Avatar
+                src={avatar}
+                icon={<UserOutlined />}
+                onClick={handleShow}
+                className="pointer"
+            />
+            <Drawer
+                closeIcon={null}
+                title={null}
+                onClose={handleClose}
+                open={open}
             >
-                <div className="sider">
-                    {collapsed || (
-                        <div className="logo">
-                            <div className="logo-text">
-                                <NavLink to="/">{LOGO_TEXT}</NavLink>
-                            </div>
-                        </div>
-                    )}
-                    <Menu
-                        className="sidebar-nav"
-                        selectedKeys={[location.pathname]}
-                        theme="dark"
-                        mode="inline"
-                        items={menuItems}
-                        onSelect={({ key }) => {
-                            navigateTo(key);
-                        }}
-                    />
-                </div>
-            </Sider>
-            <Layout className="site-layout">
-                <Header className="site-layout-header" style={{ padding: 0 }}>
-                    <Row>
-                        <Col span={12}>
-                            {React.createElement(
-                                collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
-                                {
-                                    className: 'trigger',
-                                    onClick: toggle
-                                }
-                            )}
-                        </Col>
-                        <Col span={12} style={{ paddingRight: 5 }}>
-                            <Flex gap={5} justify="flex-end">
-                                <UserMenu />
-                            </Flex>
-                        </Col>
-                    </Row>
-                </Header>
-                <Content className="site-layout-content">
-                    <Outlet />
-                </Content>
-                <Footer className="layout-footer">
-                    <div className="layout-footer-text">
-                        Copyright<sup>©</sup> {DOMAIN} {new Date().getFullYear()}
-                    </div>
-                </Footer>
-            </Layout>
-        </Layout>
+                <MenuHeading email={email} fullName={fullName} avatar={avatar} />
+                <Divider />
+                <Menu
+                    inlineIndent={6}
+                    selectedKeys={[location.pathname]}
+                    mode="inline"
+                    items={[
+                        { label: t`Your profile`, key: '/', icon: <UserOutlined /> },
+                        {
+                            label: t`Sign out`,
+                            key: `/logout`,
+                            icon: <LogoutOutlined />
+                        }
+                    ]}
+                    onSelect={({ key }) => {
+                        if (key === '/logout') {
+                            handleLogout();
+                            return;
+                        }
+                        navigateTo(key);
+                        handleClose();
+                    }}
+                />
+            </Drawer>
+        </>
     );
 }
